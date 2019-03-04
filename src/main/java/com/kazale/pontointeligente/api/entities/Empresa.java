bd327dea405cf7d342f91fd866aa339067a47e4d
Jailson Lima <jailson-jsl@hotmail.com>
2019-03-04 20:51:54 -0300

    Criando as entidades JPA(Entity)

---
+++ src/main/java/com/kazale/pontointeligente/api/entities/Empresa.java
@@ -0,0 +1,109 @@
+package com.kazale.pontointeligente.api.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable {
+
+	private static final long serialVersionUID = 409417365899898099L;
+
+	private Long id;
+	private String razaoSocial;
+	
+	private String cnpj;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+	private List<Funcionario> funcionario;
+	
+	public Empresa() {
+		
+	}
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name="razao_social", nullable = false)
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	@Column(name="cnpj", nullable=false)
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Column(name="data_cricao", nullable=false)
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Column(name="data_atualizacao", nullable=false)
+	public Date getDataAtualizcao() {
+		return getDataAtualizcao();
+	}
+
+	public void setDataAtualizcao(Date dataAtualizcao) {
+		this.dataAtualizacao = dataAtualizcao;
+	}
+
+	@OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	public List<Funcionario> getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(List<Funcionario> funcionario) {
+		this.funcionario = funcionario;
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+	
+	public void prePersist() {
+		final Date atual =  new Date();
+		dataCriacao = atual;
+		dataAtualizacao = atual;
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", dataCriacao=" + dataCriacao
+				+ ", dataAtualizcao=" + dataAtualizacao + ", funcionario=" + funcionario + "]";
+	}
+	
+	
+}
